b4c942940893be6a53c0f9783a05bb19e8560295
Tim Webster <tim.webster@contino.io>
2021-09-06 20:24:22 +0100

    fix

--- vnet.tf
+++ vnet.tf
@@ -1,6 +1,6 @@
 # VNET
 # https://www.davidc.net/sites/default/subnets/subnets.html?network=10.0.0.0&mask=16&division=23.ff3100
-resource "azurerm_virtual_network" "policy" {
+resource "azurerm_virtual_network" "dns" {
   name                = "dns-vnet"
   location            = azurerm_resource_group.dns_rg.location
   resource_group_name = azurerm_resource_group.dns_rg.name
@@ -11,14 +11,14 @@ resource "azurerm_virtual_network" "policy" {
 resource "azurerm_subnet" "bastion_sn" {
   name                 = "AzureBastionSubnet"
   resource_group_name  = azurerm_resource_group.dns_rg.name
-  virtual_network_name = azurerm_virtual_network.dns_rg.name
+  virtual_network_name = azurerm_virtual_network.dns.name
   address_prefixes     = [var.bastion_subnet_cidr]
 }
 
 resource "azurerm_subnet" "dns_sn" {
   name                 = "dns-sn"
   resource_group_name  = azurerm_resource_group.dns_rg.name
-  virtual_network_name = azurerm_virtual_network.dns_rg.name
+  virtual_network_name = azurerm_virtual_network.dns.name
   address_prefixes     = [var.dns_subnet_cidr]
 }
 
