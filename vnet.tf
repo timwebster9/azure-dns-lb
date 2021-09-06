edb71498542e02b5f52a2e647e5af64e5ac8802a
Tim Webster <tim.webster@contino.io>
2021-09-06 23:17:21 +0100

    nsg

--- vnet.tf
+++ vnet.tf
@@ -22,3 +22,25 @@ resource "azurerm_subnet" "dns_sn" {
   address_prefixes     = [var.dns_subnet_cidr]
 }
 
+resource "azurerm_network_security_group" "bastion" {
+  name                = "bastion-nsg"
+  location            = azurerm_resource_group.dns_rg.location
+  resource_group_name = azurerm_resource_group.dns_rg.name
+
+  security_rule {
+    name                       = "allow-ssh"
+    priority                   = 101
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "212.159.71.60"
+    destination_address_prefix = "*"
+  }
+}
+
+resource azurerm_subnet_network_security_group_association "bastion-nsg-assoc" {
+  subnet_id                 = azurerm_subnet.dns_sn.id
+  network_security_group_id = azurerm_network_security_group.bastion.id
+}
